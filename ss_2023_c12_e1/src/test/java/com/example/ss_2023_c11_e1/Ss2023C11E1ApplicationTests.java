5d7dba4fbdee1d0fef3067665bb7888e0f1de2e6
shouvik126 <shouvikpradhan126@gmail.com>
2023-10-08 14:09:11 +0530

    auth server part2

---
+++ ss_2023_c12_e1/src/test/java/com/example/ss_2023_c11_e1/Ss2023C11E1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ss_2023_c11_e1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss2023C11E1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
